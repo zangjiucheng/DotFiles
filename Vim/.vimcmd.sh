cc1feda70a49b334fdc1db03abce50ea4401135d
Jiucheng <zangjiucheng@gmail.com>
2023-10-22 13:17:40 -0400

    Update File Dir

--- Vim/.vimcmd.sh
+++ Vim/.vimcmd.sh
@@ -15,3 +15,8 @@ git clone https://github.com/w0rp/ale.git ~/.vim/pack/git-plugins/start/ale
 
 git clone https://github.com/github/copilot.vim.git \
   ~/.vim/pack/github/start/copilot.vim
+
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/surround.git
+vim -u NONE -c "helptags surround/doc" -c q
